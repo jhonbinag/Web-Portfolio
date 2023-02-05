613411c626f1d5808ea4d16c61636497421ea204
Jhon Binag <98508806+jhonbinag@users.noreply.github.com>
2023-02-05 13:03:39 +0800

    Update work.jsx

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import workOne from '../assets/Proj1.png';
 import workTwo from '../assets/Proj2.png';
 import workThree from '../assets/Proj3.png';
+import workFour from '../assets/Proj4.png';
 
 const work = () => {
   return (
@@ -48,6 +49,22 @@ const work = () => {
                     </div>
                 </div>
                 <div style={{backgroundImage: `url(${workThree})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    {/*Hover Effects*/}
+                    <div className="opacity-0 group-hover:opacity-100">
+                        <span className='font-2xl font-bold text-white tracking-wider'>
+                            WordPress Project
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://www.lightchangescoaching.com/' target='_blank'>
+                                <buttons className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</buttons>
+                            </a>
+                            <a href='/'>
+                                <buttons className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</buttons>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage: `url(${workFour})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                     {/*Hover Effects*/}
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className='font-2xl font-bold text-white tracking-wider'>
